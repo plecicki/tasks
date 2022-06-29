68e7059cba8fdfd055e44eb0c2c075cfe04f7f90
PLecicki <p.lecicki@gmail.com>
2022-06-29 23:03:05 +0200

    Create controller

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "Title1", "Content1");
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+
+    }
+
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(2L, "Title2", "Content2");
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
