0eb8531ddbbabd42f370b02e78097ecc2840a535
PLecicki <p.lecicki@gmail.com>
2022-10-05 10:56:19 +0200

    Edit button in mail template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -16,6 +16,8 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://plecicki.github.io/");
+        context.setVariable("button", "Visit website");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
